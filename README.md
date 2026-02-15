ecbdc6bb08c8e0fdd5b9839d366d801757a4c3da
Prince <princeshamsuzzaman@gmail.com>
2026-02-15 19:28:40 +0600

    Initial commit: Temperature & Humidity IoT Node

---
+++ README.md
@@ -0,0 +1,14 @@
+﻿# Temperature & Humidity IoT Node
+
+ESP8266 Blynk node broadcasting ambient temperature and humidity with remote buzzer control.
+
+## Original Folder
+06_Agriculture_and_Environment\Temperature_Humidity_IoT
+
+## Hardware
+ESP8266 NodeMCU, DHT sensor, buzzer.
+
+## Notes
+Update template IDs and Wi-Fi credentials before deploying to production.
+
+Imported from Electronics project on 2026-02-15.
---
+++ Temperature_Humidity_IoT.ino
@@ -0,0 +1,59 @@
+#define BLYNK_TEMPLATE_ID "TMPLUPQcSvSs"
+#define BLYNK_DEVICE_NAME "DHT11"
+#define BLYNK_AUTH_TOKEN "u7hvulUrXBbmrCx92Pvthk6kNiqXeZK6"
+
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h> 
+#include <BlynkSimpleEsp8266.h>
+
+#include <DHT.h>
+
+
+char auth[] = "u7hvulUrXBbmrCx92Pvthk6kNiqXeZK6";
+
+char ssid[] = "Sadat";
+char pass[] = "#s@d@t##";
+
+BlynkTimer timer;
+
+
+#define DHTPIN 4 //Connect Out pin to D2 in NODE MCU
+#define DHTTYPE DHT11  
+DHT dht(DHTPIN, DHTTYPE);
+
+
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+    Blynk.virtualWrite(V0, t);
+    Blynk.virtualWrite(V1, h);
+    Serial.print("Temperature : ");
+    Serial.print(t);
+    Serial.print("    Humidity : ");
+    Serial.println(h);
+}
+void setup()
+{   
+  
+  Serial.begin(115200);
+  
+
+  Blynk.begin(auth, ssid, pass);
+  dht.begin();
+  timer.setInterval(100L, sendSensor);
+ 
+  }
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+ }
